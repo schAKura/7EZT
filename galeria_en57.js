f8547fe90ab92e452b515812be286ce020242bc9
schAKura <alan.kuranowski@tm1.edu.pl>
2025-01-13 13:24:03 +0100

    modified:   galeria_en57.js 	new file:   media/en57/foto (37).jpg 	new file:   media/en57/foto (38).jpg

--- galeria_en57.js
+++ galeria_en57.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Tablica z nazwami obrazów
     const images = [];
-    for (let i = 1; i <= 36; i++) {
+    for (let i = 1; i <= 38; i++) {
         images.push(`media/en57/foto (${i}).jpg`);
     }
     
