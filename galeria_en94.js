22e5ccbad19328268d1cc11804f63b00f0e9f216
schAKura <alan.kuranowski@tm1.edu.pl>
2025-01-13 22:33:58 +0100

    modified:   elf.htm 	modified:   en57.htm 	modified:   en71.htm 	modified:   en94.htm 	modified:   ew60.htm 	modified:   flirt.htm 	new file:   galeria_en94.js 	modified:   impuls.htm 	modified:   index.htm 	new file:   media/en94/foto (1).jpg 	new file:   media/en94/foto (10).jpg 	new file:   media/en94/foto (11).jpg 	new file:   media/en94/foto (12).jpg 	new file:   media/en94/foto (13).jpg 	new file:   media/en94/foto (14).jpg 	new file:   media/en94/foto (15).jpg 	new file:   media/en94/foto (16).jpg 	new file:   media/en94/foto (17).jpg 	new file:   media/en94/foto (2).jpg 	new file:   media/en94/foto (3).jpg 	new file:   media/en94/foto (4).jpg 	new file:   media/en94/foto (5).jpg 	new file:   media/en94/foto (6).jpg 	new file:   media/en94/foto (7).jpg 	new file:   media/en94/foto (8).jpg 	new file:   media/en94/foto (9).jpg 	modified:   style.css

---
+++ galeria_en94.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let slideIndex = 0;
+    const slideContainer = document.querySelector('.slideshow-container');
+    const dotsContainer = document.querySelector('.dots-container');
+
+    // Funkcja do stworzenia slajdów i kropek
+    function createSlides(images) {
+        images.forEach((src, index) => {
+            const slideDiv = document.createElement('div');
+            slideDiv.classList.add('mySlides', 'fade');
+
+            const img = document.createElement('img');
+            img.src = src;
+            img.alt = 'Zdjęcie EN94';
+            img.addEventListener('click', function() {
+                window.open(src, '_blank');
+            });
+
+            slideDiv.appendChild(img);
+            slideContainer.appendChild(slideDiv);
+
+            const dot = document.createElement('span');
+            dot.classList.add('dot');
+            dotsContainer.appendChild(dot);
+        });
+    }
+
+    // Tablica z nazwami obrazów
+    const images = [];
+    for (let i = 1; i <= 17; i++) {
+        images.push(`media/en94/foto (${i}).jpg`);
+    }
+    
+    createSlides(images);
+
+    const slides = document.querySelectorAll('.mySlides');
+    const dots = document.querySelectorAll('.dot');
+
+    function showSlides() {
+        slides.forEach((slide) => {
+            slide.style.display = 'none';
+        });
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = 'block';
+        dots.forEach((dot) => {
+            dot.className = dot.className.replace(' active', '');
+        });
+        dots[slideIndex - 1].className += ' active';
+        setTimeout(showSlides, 2000); // Zmiana obrazów co 2 sekundy
+    }
+
+    showSlides();
+});
